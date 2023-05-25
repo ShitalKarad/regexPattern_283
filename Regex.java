af51afaab34f8be24f10c0a3b8f34e7a250da30a
ShitalKarad <shitalkarad24@gmail.com>
2023-05-25 18:48:52 +0530

    Added:valid first name

---
+++ Regex.java
@@ -0,0 +1,17 @@
+package com.brigelabz;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+	 public  void validFirstName() {
+         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+        Matcher matcher = pattern.matcher("Shital");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
+
+}
---
+++ RegexMain.java
@@ -0,0 +1,10 @@
+package com.brigelabz;
+
+public class RegexMain {
+	public static void main(String[] args) {
+		
+		Regex regex = new Regex();
+		regex.validFirstName();
+	}
+
+}
